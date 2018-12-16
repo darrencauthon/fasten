e910603d5ded2c5980145ceb59f485ae81a01aae
Caleb Cauthon <calebcauthon@gmail.com>
2018-12-16 11:04:35 -0600

    When the API creates an event, show it on the website

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,18 @@ class EventsController < ApplicationController
   end
 
   def create
-    event = Event.create message: 'the event of a lifetime.'
+    event = create_event params[:message]
+    publish event
+
     render plain: event.to_json
+  end
+
+  def create_event(message)
+    Event.create message: params[:message]
+  end
+
+  def publish(event)
+    channels_client = Pusher::Client.new(app_id: 'fasten', key: 'app_key', secret: 'secret', host: 'poxa', port: 8080)
+    channels_client.trigger('channel', 'event', message: event.message);
   end
 end
