9fd3c0777c7ad091d765c166ac2c2eb7787d3ee1
Caleb Cauthon <calebcauthon@gmail.com>
2018-12-22 13:00:30 -0600

    Use fire/recieve for event handling

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,5 @@
+require_relative '../models/event_handler.rb'
+
 class EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
@@ -9,15 +11,15 @@ class EventsController < ApplicationController
   end
 
   def create
-    event = Apple.new.fire {message: params[:message]}
+    event = Apple.new.fire({ message: params[:message] })
     publish event
-    
+
     event = Orange.new.receive event
     publish event
-    
+
     event = Banana.new.receive event
     publish event
-    
+
     event = Pear.new.receive event
     publish event
 
@@ -25,7 +27,7 @@ class EventsController < ApplicationController
   end
 
   private
-  
+
   def publish(event)
     channels_client = Pusher::Client.new(app_id: 'fasten', key: 'app_key', secret: 'secret', host: 'poxa', port: 8080)
     channels_client.trigger('channel', 'event', message: event.message, prior_event_id: event.prior_event_id, id: event.id);
