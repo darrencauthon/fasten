33ea07edddd74fc406479eca8e3444bed7b4c408
Darren Cauthon <darren@cauthon.com>
2018-12-27 09:07:21 -0600

    Drop the commented code.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,13 +23,6 @@ class EventsController < ApplicationController
       i[:type].constantize.new.send(i[:method], k)
     end
 
-    #steps.each_with_index do |step, i|
-    #  event =  (i == 0)
-    #           ? step[:type].constantize.new.fire(originating_data)
-    #           : step[:type].constantize.new.receive(event)
-    #  publish event
-    #end
-
     render plain: event.to_json
   end
 
