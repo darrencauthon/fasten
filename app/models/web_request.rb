5c56f8a58a8b3a5a754a1f2a249f6f74bb639e37
Darren Cauthon <darren@cauthon.com>
2018-12-28 16:51:55 -0600

    Ignore the body for now.

--- app/models/web_request.rb
+++ app/models/web_request.rb
@@ -5,8 +5,8 @@ class WebRequest
     Event.new message: "test #{response.reason_phrase}", data: { 
       status: response.status,
       reason_phrase: response.reason_phrase,
-      response_headers: response.headers,
-      body: response.body
+      response_headers: response.headers
+      #body: response.body
     }
   end
 
