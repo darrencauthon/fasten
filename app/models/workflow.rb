6e525fc201f52fa5aefe37264205d2a18d9fc162
Darren Cauthon <darren@cauthon.com>
2018-12-29 10:23:08 -0600

    Create the idea of a workflow.

---
+++ app/models/workflow.rb
@@ -0,0 +1,37 @@
+class Workflow
+
+  attr_accessor :steps
+
+  def self.build(definition)
+    workflow = Workflow.new
+    workflow.steps = definition[:steps]
+    workflow
+  end
+
+  def start(data)
+
+    steps.each_with_index { |x, i| x[:method] = (i == 0 ? :fire : :receive) }
+
+    steps.reduce(data) do |last_event, step_data|
+      e = step_data[:type].constantize.new.send(step_data[:method], last_event)
+
+      e.prior_event_id = last_event.id if last_event.is_a?(Event)
+      e.data = e.data || {}
+      e.save
+      
+      publish e
+      
+      e
+    end
+
+  end
+
+  private
+
+  def publish(event)
+    channels_client = Pusher::Client.new(app_id: 'fasten', key: 'app_key', secret: 'secret', host: 'poxa', port: 8080)
+    data = { message: event.message, prior_event_id: event.prior_event_id, id: event.id }
+    channels_client.trigger('channel', 'event', data);
+  end
+
+end
