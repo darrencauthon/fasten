97fbd956ba8ab71feb7e06ead50422f7f15c0856
Darren Cauthon <darren@cauthon.com>
2018-12-28 23:12:58 -0600

    Cleanup.

--- app/models/event_handler.rb
+++ app/models/event_handler.rb
@@ -1,14 +1,10 @@
 class EventHandler
 
   def receive(event)
-    self.create_event message: "event ##{event.id} was fired"
+    Event.new message: "event ##{event.id} was fired"
   end
 
   def fire(data)
-    self.create_event message: data[:message]
-  end
-
-  def create_event(data)
     Event.new message: data[:message]
   end
 
