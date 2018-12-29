5353a963138213743dde3df2ccfaa4beef64e8aa
Darren Cauthon <darren@cauthon.com>
2018-12-28 23:46:02 -0600

    Using this middleware allows us to get the response body.

--- app/models/web_request.rb
+++ app/models/web_request.rb
@@ -1,12 +1,20 @@
 class WebRequest
 
   def fire(data)
-    response = Faraday.get data[:url]
+    url = data[:url]
+
+    conn = Faraday.new do |connection|
+      connection.response :encoding
+      connection.adapter Faraday.default_adapter
+    end
+
+
+    response = conn.get url
     Event.new message: "#{data[:url]} reported #{response.status}", data: { 
       status: response.status,
       reason_phrase: response.reason_phrase,
-      response_headers: response.headers
-      #body: response.body
+      response_headers: response.headers,
+      body: response.body
     }
   end
 
