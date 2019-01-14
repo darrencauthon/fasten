a3923a34a6ee3e281b2f84231017fdcd5dba84fb
Darren Cauthon <darren@cauthon.com>
2019-01-13 20:48:26 -0600

    Set event data to a symbolized hash.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ApplicationRecord
   serialize :data, JSON
+
+  after_initialize do |event|
+    event.data = SymbolizedHash.new event.data
+  end
+
+  after_find do |event|
+    event.data = SymbolizedHash.new event.data
+  end
 end
