a9c37c5549bbe4e54e7ead44f20d73a7f975ddbe
Darren Cauthon <darren@cauthon.com>
2019-01-18 15:56:32 -0600

    Refactor.

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -3,12 +3,15 @@ class Trigger
   attr_accessor :config
 
   def receive event
-    path  = config[:rules][0][:path]
-    match = config[:rules][0][:value].to_s
-    value = event.data[path].to_s
-    if (match == value)
+
+    path     = config[:rules][0][:path]
+    match_to = config[:rules][0][:value].to_s
+    value    = event.data[path].to_s
+
+    if (value == match_to)
       Event.new message: "#{match}=#{value}", data: event.data
     end
+
   end
 
 end
