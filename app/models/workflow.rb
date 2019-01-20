54ae8b27d7009027aacce7861862c077bd61d041
Darren Cauthon <darren@cauthon.com>
2019-01-20 16:02:11 -0600

    Some fields should not be mashed.

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -26,8 +26,12 @@ class Workflow
   end
 
   def self.mash(config, event)
+
+    fields_not_to_mash = []
+
     config
       .select { |_, y| y.is_a? String }
+      .reject { |x, _| fields_not_to_mash.include? x }
       .each do |key, value|
         config[key] = mash_single_value(value, event)
       end
