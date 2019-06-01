e41c1f6bc52b0c59229e97441a941328ab01f31a
Darren Cauthon <darren@cauthon.com>
2019-06-01 08:14:33 -0500

    Push this javascript into a single object.

--- app/assets/javascripts/event-visualization.js
+++ app/assets/javascripts/event-visualization.js
@@ -1,92 +1,99 @@
-function createBlankNetworkDiagram(elementId) {
-  const nodes = new vis.DataSet([]);
-  const edges = new vis.DataSet([]);
-  var container = document.getElementById(elementId);
-  var data = {
-    nodes: nodes,
-    edges: edges
+var Elephant = function()
+{
+
+  this.createBlankNetworkDiagram = function(elementId) {
+    const nodes = new vis.DataSet([]);
+    const edges = new vis.DataSet([]);
+    var container = document.getElementById(elementId);
+    var data = {
+      nodes: nodes,
+      edges: edges
+    };
+    var options = {};
+    var network = new vis.Network(container, data, options);
+
+    return { network, nodes, edges }
   };
-  var options = {};
-  var network = new vis.Network(container, data, options);
 
-  return { network, nodes, edges }
-}
+  this.showEventBubbles = function(diagram, events) {
+    events.forEach(thisEvent => {
+      addEventInstanceAsIndividualNode(diagram, thisEvent);
+    });
+  };
 
-function showEventBubbles(diagram, events) {
-  events.forEach(thisEvent => {
-    addEventInstanceAsIndividualNode(diagram, thisEvent);
-  });
-}
+  this.addEventInstanceAsIndividualNode = function (diagram, event) {
+    diagram.edges.add({ from: event.id, to: event.prior_event_id });
 
-function addEventInstanceAsIndividualNode(diagram, event) {
-  diagram.edges.add({ from: event.id, to: event.prior_event_id });
+    diagram.nodes.add({
+      id: event.id,
+      label: event.message,
+      priorEventId: event.prior_event_id
+    });
+  };
 
-  diagram.nodes.add({
-    id: event.id,
-    label: event.message,
-    priorEventId: event.prior_event_id
-  });
-}
+  this.addEventInstanceAsAggregateNodes = function() {
+    const stepGuidMap = new Map();
 
-var addEventInstanceAsAggregateNodes = function() {
-  const stepGuidMap = new Map();
+    return function(diagram, event) {
+      if (stepGuidMap.get(event.step_guid)) {
+        const node = stepGuidMap.get(event.step_guid);
+        node.count++;
+        node.label = `Step: #${event.step_guid} ${node.count}`
+        diagram.nodes.update(node);
+        return;
+      }
 
-  return function(diagram, event) {
-    if (stepGuidMap.get(event.step_guid)) {
-      const node = stepGuidMap.get(event.step_guid);
-      node.count++;
-      node.label = `Step: #${event.step_guid} ${node.count}`
-      diagram.nodes.update(node);
-      return;
+      const node = {
+        id: event.step_guid,
+        label: `Step: #${event.step_guid} 0`,
+        count: 0
+      };
+      stepGuidMap.set(event.step_guid, node);
+      diagram.nodes.add(node);
+    };
+  }();
+
+  this.buildStepNodeObject = function(step, properties) {
+    const { name, type, guid, next_steps } = step;
+    return Object.assign({
+      id: diagram.nodes.length,
+      label: `${name}:${type}:${guid}`,
+      widthConstraint: 60,
+      shape: 'box',
+      step: step
+    }, properties);
+  };
+
+  this.buildLine = function(fromNode, toNode) {
+    return {
+      from: fromNode.id,
+      to: toNode.id,
+      arrows: { to: true }
+    }
+  };
+
+  this.addStepAsNode = function(diagram, step, parentNode) {
+    const { name, type, guid, next_steps } = step;
+    const newNode = buildStepNodeObject(step, { id: diagram.nodes.length });
+
+    if (parentNode) {
+      const newEdge = buildLine(parentNode, newNode);
+      diagram.edges.add(newEdge);
+      newNode.parentNode = parentNode;
     }
 
-    const node = {
-      id: event.step_guid,
-      label: `Step: #${event.step_guid} 0`,
-      count: 0
+    diagram.nodes.add(newNode);
+
+    if (step.next_steps) {
+      const nextSteps = step.next_steps.forEach(step => addStepAsNode(diagram, step, newNode));
+      return [newNode].concat(nextSteps);
+    } else {
+      return [newNode];
     }
-    stepGuidMap.set(event.step_guid, node);
-    diagram.nodes.add(node);
+
+    return newNode;
   };
-}();
 
-function buildStepNodeObject(step, properties) {
-  const { name, type, guid, next_steps } = step;
-  return Object.assign({
-    id: diagram.nodes.length,
-    label: `${name}:${type}:${guid}`,
-    widthConstraint: 60,
-    shape: 'box',
-    step: step
-  }, properties);
-}
+  return this;
 
-function buildLine(fromNode, toNode) {
-  return {
-    from: fromNode.id,
-    to: toNode.id,
-    arrows: { to: true }
-  }
-}
-
-function addStepAsNode(diagram, step, parentNode) {
-  const { name, type, guid, next_steps } = step;
-  const newNode = buildStepNodeObject(step, { id: diagram.nodes.length });
-
-  if (parentNode) {
-    const newEdge = buildLine(parentNode, newNode);
-    diagram.edges.add(newEdge);
-    newNode.parentNode = parentNode;
-  }
-
-  diagram.nodes.add(newNode);
-
-  if (step.next_steps) {
-    const nextSteps = step.next_steps.forEach(step => addStepAsNode(diagram, step, newNode));
-    return [newNode].concat(nextSteps);
-  } else {
-    return [newNode];
-  }
-
-  return newNode;
-}
+};
