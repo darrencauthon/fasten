c9946f2f896d1d8fe0557a73435f1ea1eba72203
Darren Cauthon <darren@cauthon.com>
2019-06-14 22:12:51 -0500

    Read from a workflows directory.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -86,7 +86,7 @@ class EventsController < ApplicationController
 
   def read_a_file
     content = ''
-    File.open('/test/test.txt') do |f|
+    File.open("/workflows/#{params[:file]}") do |f|
       f.each_line do |line|
        content = content + line
       end
