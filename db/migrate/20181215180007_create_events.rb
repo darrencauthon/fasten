bdb17fcbe7448c0b8be0545c2fd15f784d1ca811
Darren Cauthon <darren@cauthon.com>
2019-07-13 20:47:10 -0500

    Switch to a string primary key.

--- db/migrate/20181215180007_create_events.rb
+++ db/migrate/20181215180007_create_events.rb
@@ -1,6 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
-    create_table :events do |t|
+    create_table :events, id: false do |t|
+      t.string :id, primary_key: true
       t.string :message
 
       t.timestamps
