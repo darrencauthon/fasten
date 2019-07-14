c6713e75312d4fb2a03c8e2dd5db6e106f1c8bcd
Darren Cauthon <darren@cauthon.com>
2019-07-13 23:07:57 -0500

    Display a workflow.

--- app/controllers/workflow_editor_controller.rb
+++ app/controllers/workflow_editor_controller.rb
@@ -3,11 +3,16 @@ class WorkflowEditorController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    @id = params[:id]
     render layout: 'boko'
   end
 
   def data
-    render json: {}
+    content = ''
+    File.open("/workflows/#{params[:id]}.json") do |f|
+      f.each_line { |l| content = content + l }
+    end
+    render json: { workflow: JSON.parse(content) }
   end
 
 end
