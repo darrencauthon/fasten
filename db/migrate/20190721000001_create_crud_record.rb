a5976e4700c8a07d894fcc75096bd30066f6f28f
Darren Cauthon <darren@cauthon.com>
2019-07-22 09:28:58 -0500

    This is how we will save records.

---
+++ db/migrate/20190721000001_create_crud_record.rb
@@ -0,0 +1,12 @@
+class CreateCrudRecord < ActiveRecord::Migration[5.2]
+  def change
+    create_table :crud_records, id: false do |t|
+      t.string :id, primary_key: true
+      t.string :collection_name
+      t.string :record_id
+      t.text :data
+
+      t.timestamps
+    end
+  end
+end
