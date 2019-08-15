82bbb2c5705b80ae11332f9c7810a896dcda38f8
Darren Cauthon <darren@cauthon.com>
2019-08-14 21:37:44 -0500

    Parse through the matches.

--- app/models/html_parser.rb
+++ app/models/html_parser.rb
@@ -7,14 +7,31 @@ class HtmlParser
     doc = Nokogiri::HTML(event.data[config[:path]])
 
     matches = {}
-
-    config['extract'].each_with_index do |extract, index|
-      matches[index] = doc.css(extract['css']).map { |x| x.xpath(extract['value']).to_s }
+    config['extract'].keys.each_with_index do |key, index|
+      extract = config['extract'][key]
+      matches[key] = doc.css(extract['css']).map { |x| x.xpath(extract['value']).to_s }
     end
 
-    matches.keys.map do |key|
-      Event.new data: matches[key]
+    data = []
+    matches[matches.keys.first].each_with_index do |x, i|
+      record = {}
+      matches.keys.each do |key|
+        record[key] = matches[key][i]
+      end
+      data << record
     end
+
+    Event.new data: data
+
+    #matches = {}
+
+    #config['extract'].each_with_index do |extract, index|
+      #matches[index] = doc.css(extract['css']).map { |x| x.xpath(extract['value']).to_s }
+    #end
+
+    #matches.keys.map do |key|
+      #Event.new data: matches[key]
+    #end
 
   end
 
