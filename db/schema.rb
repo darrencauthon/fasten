f8220eb8c807a0ca21aeca477c2b90c6642a63f4
Darren Cauthon <darren@cauthon.com>
2019-08-16 12:41:39 -0500

    Finish adding workflow id to events.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_000001) do
+ActiveRecord::Schema.define(version: 2019_08_16_000001) do
 
   create_table "crud_records", id: :string, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "collection_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_21_000001) do
     t.text "data", limit: 4294967295
     t.string "step_id"
     t.string "run_id"
+    t.string "workflow_id"
   end
 
 end
