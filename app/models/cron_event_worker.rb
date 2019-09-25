dbd0fdc0db48b4fae916a14a8e2861e9b025252b
Darren Cauthon <darren@cauthon.com>
2019-09-24 19:52:58 -0500

    Starting a new run.

--- app/models/cron_event_worker.rb
+++ app/models/cron_event_worker.rb
@@ -3,7 +3,12 @@ class CronEventWorker
   include Sidekiq::Worker
 
   def perform workflow_id, step_id
-    raise [workflow_id, step_id].inspect
+    workflow = Workflow.find workflow_id
+    step = workflow.steps.select { |x| x[:id] == step_id }.first
+
+    originating_event = Event.new(data: step[:config] || {})
+
+    run = Run.start originating_event, step, workflow
   end
 
 end
