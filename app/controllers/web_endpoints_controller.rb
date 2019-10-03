44aaa757e8271131f5c4a21229c40cc9f66c1218
Darren Cauthon <darren@cauthon.com>
2019-10-02 21:47:54 -0500

    Refactor.

--- app/controllers/web_endpoints_controller.rb
+++ app/controllers/web_endpoints_controller.rb
@@ -18,19 +18,28 @@ class WebEndpointsController < ApplicationController
   end
 
   def fire
-    headers = {}
-    request.headers.each do |x|
-      headers[x[0]] = x[1] if x[1].is_a?(String)
-    end
-    data = {
+    render json: get_the_data
+  end
+
+  private
+
+  def get_the_data
+    {
       url: request.env['PATH_INFO'],
       method: request.env['REQUEST_METHOD'],
       form: request.env['rack.request.form_hash'],
       query: request.env['rack.request.query_hash'],
       raw: request.raw_post,
-      headers: headers
+      headers: get_the_headers
     }
-    render json: data
+  end
+
+  def get_the_headers
+    headers = {}
+    request.headers.each do |x|
+      headers[x[0]] = x[1] if x[1].is_a?(String)
+    end
+    headers
   end
 
 end
