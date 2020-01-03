f50a20dcf2789f3ae1b411849d1157d8f90d6fae
Darren Cauthon <darren@cauthon.com>
2020-01-02 21:36:18 -0600

    Allow a limit parameter.

--- overt/app/models/all_records.rb
+++ overt/app/models/all_records.rb
@@ -3,10 +3,15 @@ class AllRecords
   attr_accessor :config
 
   def receive(event)
-    CrudRecord
-      .where(collection_name: config[:collection])
+
+    records = CrudRecord.where(collection_name: config[:collection])
+
+    records = records.limit(config[:limit]) if config[:limit].to_i > 0
+
+    records
       .map { |x| { id: x.id, name: x.name, data: x.data } }
       .map { |x| Event.new data: x }
+
   end
 
 end
