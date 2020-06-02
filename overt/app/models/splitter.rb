01103e915895e415b4ae9ac5f4286001af2bf866
Darren Cauthon <darren@cauthon.com>
2020-06-02 15:13:13 -0500

    Add the ability to limit the results from the splitter.

--- overt/app/models/splitter.rb
+++ overt/app/models/splitter.rb
@@ -3,7 +3,13 @@ class Splitter
   attr_accessor :config
 
   def receive(event)
-    Mashing.dig config[:path], event.data
+    events = Mashing.dig config[:path], event.data
+
+    if config[:limit] && config[:limit].to_s == config[:limit].to_i.to_s
+      events = events.take config[:limit]
+    end
+
+    events
   end
 
 end
