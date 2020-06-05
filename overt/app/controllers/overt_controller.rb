bd00f370cc754986c7953a082c137a19d314ea80
Darren Cauthon <darren@cauthon.com>
2020-06-05 07:55:07 -0500

    Show event children.

--- overt/app/controllers/overt_controller.rb
+++ overt/app/controllers/overt_controller.rb
@@ -195,8 +195,10 @@ class OvertController < ApplicationController
   def view_event
     @event = Event.find params[:id]
 
-    @parent_event = @event ? Event.where(parent_event_id: @event.parent_event_id).first : nil
-    @parent_event = nil if @parent_event.id == @event.id
+    @parent_event = @event ? Event.where(id: @event.parent_event_id).first : nil
+
+    @child_events = Event.where(parent_event_id: @event.id)
+                         .paginate(page: params[:page], per_page: 10)
 
     render layout: 'water'
   end
