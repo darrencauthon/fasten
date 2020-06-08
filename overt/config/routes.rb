9f9704e1630c746a652a9745792570eca405a496
Darren Cauthon <darren@cauthon.com>
2020-06-08 07:31:32 -0500

    Drop the old events code.

--- overt/config/routes.rb
+++ overt/config/routes.rb
@@ -42,7 +42,6 @@ Rails.application.routes.draw do
 
   #############################################
 
-  get 'events', to: 'events#new_index'
   get 'workflows', to: 'workflows#index'
   get 'workflows/json/:id', to: 'workflows#json'
   post 'workflows/json/:id', to: 'workflows#save'
@@ -56,37 +55,13 @@ Rails.application.routes.draw do
   get 'runs', to: 'runs#index'
   get 'runs/view/:id', to: 'runs#view'
 
-  get 'records', to: 'crud#index'
-  get 'records/view/:id', to: 'crud#view'
-  get 'records/json/:id', to: 'crud#json'
-  post 'records/delete', to: 'crud#delete'
-
   get 'sequence/new'
-  get 'events/index'
-  get 'events/all'
-  get 'events/single/:id', to: 'events#single'
-  get 'events/aggregate'
-  post 'events/create'
-  get 'events/create'
-  get 'events/demo'
-
-  post 'events/create_web'
-  get 'events/create_web'
-
-  post 'events/create_with_tree'
-  get 'events/create_with_tree'
-
-  post 'events/import_definition'
-
-  post 'events/load_workflow_and_run'
 
   get 'step_editor/index'
 
   get 'workflow_editor_data/:id', to: "workflow_editor#data"
   get 'workflow_editor/:id', to: "workflow_editor#index"
   post 'workflow_editor/:id', to: "workflow_editor#save"
-
-  get 'cron_events', to: 'cron_events#index'
 
   get 'web_endpoints', to: 'web_endpoints#index'
 
