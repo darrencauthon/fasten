f8428eb3dbfaab2923dd13fcaf69bd114939cd9f
Darren Cauthon <darren@cauthon.com>
2020-07-05 19:08:39 -0500

    Drop the workflow editor.

--- overt/config/routes.rb
+++ overt/config/routes.rb
@@ -54,10 +54,6 @@ Rails.application.routes.draw do
 
   get 'step_editor/index'
 
-  get 'workflow_editor_data/:id', to: "workflow_editor#data"
-  get 'workflow_editor/:id', to: "workflow_editor#index"
-  post 'workflow_editor/:id', to: "workflow_editor#save"
-
   get 'web_endpoints', to: 'web_endpoints#index'
 
   mount Sidekiq::Web => '/sidekiq'
